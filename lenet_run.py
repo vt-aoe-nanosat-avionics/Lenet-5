6c56fee10703505ce11f5d8b7d3c49d7b419f28f
andrewmcg1 <greekgeek191@gmail.com>
2024-11-11 13:23:21 -0500

    added tensorflow implementation of lenet5

---
+++ lenet_run.py
@@ -0,0 +1,46 @@
+import os
+
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow.keras import datasets, layers, models, losses
+
+
+(x_train,y_train),(x_test,y_test) = datasets.mnist.load_data()
+print(x_train.shape)
+
+x_train = tf.pad(x_train, [[0, 0], [2,2], [2,2]])/255
+x_test = tf.pad(x_test, [[0, 0], [2,2], [2,2]])/255
+print(x_train.shape)
+
+x_train = tf.expand_dims(x_train, axis=3, name=None)
+x_test = tf.expand_dims(x_test, axis=3, name=None)
+print(x_train.shape)
+
+x_val = x_train[-2000:,:,:,:] 
+y_val = y_train[-2000:] 
+x_train = x_train[:-2000,:,:,:] 
+y_train = y_train[:-2000]
+
+
+model = models.Sequential()
+model.add(layers.Conv2D(6, 5, activation='tanh', input_shape=x_train.shape[1:]))
+model.add(layers.AveragePooling2D(2))
+model.add(layers.Activation('sigmoid'))
+model.add(layers.Conv2D(16, 5, activation='tanh'))
+model.add(layers.AveragePooling2D(2))
+model.add(layers.Activation('sigmoid'))
+model.add(layers.Conv2D(120, 5, activation='tanh'))
+model.add(layers.Flatten())
+model.add(layers.Dense(84, activation='tanh'))
+model.add(layers.Dense(10, activation='softmax'))
+model.summary()
+
+model.compile(optimizer='adam', loss=losses.sparse_categorical_crossentropy, metrics=['accuracy'])
+
+
+model.load_weights("training_1/cp.weights.h5")
+
+model.evaluate(x_test, y_test)
